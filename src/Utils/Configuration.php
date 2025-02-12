c10e26b7b7c7647c3e5820508048304b3ea40f14
FGDumitru|B <xelotx@gmail.com>
2025-02-12 19:01:17 +0200

    WIP.

--- src/Utils/Configuration.php
+++ src/Utils/Configuration.php
@@ -1,7 +1,34 @@
 <?php
 
 namespace FGDumitru\LlamaManager\Utils;
+
+use Exception;
+use Symfony\Component\Yaml\Yaml;
+
 class Configuration
 {
+    /**
+     * @var mixed
+     */
+    private static $configuration = null;
+
+    /**
+     * @throws Exception
+     */
+    public static function readConfiguration() {
+
+        if (!is_null(self::$configuration)) {
+            return self::$configuration;
+        }
+
+        if (!file_exists("config.yml")) {
+            throw new Exception("config.yml file does not exist.");
+        } else {
+            // Load and parse the config.yml file
+            self::$configuration = Yaml::parseFile("config.yml");
+        }
+
+        return self::$configuration;
+    }
 
 }
--- src/Utils/Updater.php
+++ src/Utils/Updater.php
@@ -2,10 +2,75 @@
 
 namespace FGDumitru\LlamaManager\Utils;
 
+use Exception;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Exception\RequestException;
+
 class Updater
 {
-    public function execute()
+    public function execute(): void
     {
+        $this->checkLatestVersion();
         echo "Updater executed successfully!\n";
     }
+
+    private function checkLatestVersion(): void
+    {
+        $entries = Configuration::readConfiguration();
+
+        foreach ($entries as $key => $entry) {
+
+            // Check if the updates have been disabled for this git project. Idem if the entry does not exist.
+            if (!$entry['check-updates'] ?? false) {
+                continue;
+            }
+
+            $gitReleasesLink = $entry['git-releases'];
+
+            try {
+                $entriesArray = $this->fetchGitHubReleases($gitReleasesLink);
+
+                $latestRelease = reset($entriesArray);
+
+                var_dump($latestRelease);
+
+            } catch (Exception $e) {
+                echo 'Error: ' . $e->getMessage();
+            }
+
+        }
+
+
+    }
+
+    /**
+     * @throws GuzzleException
+     * @throws Exception
+     */
+    function fetchGitHubReleases($url) {
+        $client = new Client();
+
+        try {
+            // Make a GET request to the GitHub API
+            $response = $client->request('GET', $url, [
+                'headers' => [
+                    'User-Agent' => 'PHP'
+                ]
+            ]);
+
+            // Get the body of the response and decode it as JSON
+            $releasesJson = json_decode($response->getBody(), true);
+
+            if (json_last_error() !== JSON_ERROR_NONE) {
+                throw new Exception("JSON Decode Error: " . json_last_error_msg());
+            }
+
+            return $releasesJson;
+        } catch (RequestException $e) {
+            // Handle any errors that occur during the request
+            throw new Exception("Guzzle HTTP Error: " . $e->getMessage());
+        }
+    }
+
 }
